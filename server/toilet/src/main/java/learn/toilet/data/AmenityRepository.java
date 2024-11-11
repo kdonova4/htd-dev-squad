0f478a4e9235746dddc0f62a574a2b707f1ffb25
Stan Choi <sjc591@nyu.edu>
2024-11-11 13:32:00 -0500

    Rename user repository files to start with AppUser and add interfaces for Amenity, AppUser, RestroomAmenity

--- server/toilet/src/main/java/learn/toilet/data/AmenityRepository.java
+++ server/toilet/src/main/java/learn/toilet/data/AmenityRepository.java
@@ -1,10 +1,20 @@
 package learn.toilet.data;
 
 import learn.toilet.models.Amenity;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface AmenityRepository {
+
+    List<Amenity> amenities();
+
     Amenity findById(int amenityId);
 
     Amenity add(Amenity amenity);
 
+    boolean update(Amenity amenity);
+
+    @Transactional
+    boolean deleteById(int amenityId);
 }
--- server/toilet/src/main/java/learn/toilet/data/RestroomAmenityRepository.java
+++ server/toilet/src/main/java/learn/toilet/data/RestroomAmenityRepository.java
@@ -1,4 +1,11 @@
 package learn.toilet.data;
 
+import learn.toilet.models.RestroomAmenity;
+
 public interface RestroomAmenityRepository {
+    boolean add(RestroomAmenity restroomAmenity);
+
+    boolean update(RestroomAmenity restroomAmenity);
+
+    boolean deleteByKey(int restroomId, int amenityId);
 }
