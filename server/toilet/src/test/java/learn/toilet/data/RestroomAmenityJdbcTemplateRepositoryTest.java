c980d8eb117518546a11dcc3ab599cac23bbc08f
nick <nlimurphy@gmail.com>
2024-11-11 12:01:08 -0700

    added restroom data tests and restroom amenity data tests

--- server/toilet/src/test/java/learn/toilet/data/RestroomAmenityJdbcTemplateRepositoryTest.java
+++ server/toilet/src/test/java/learn/toilet/data/RestroomAmenityJdbcTemplateRepositoryTest.java
@@ -1,7 +1,58 @@
 package learn.toilet.data;
 
+import learn.toilet.models.RestroomAmenity;
+import learn.toilet.models.Amenity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class RestroomAmenityJdbcTemplateRepositoryTest {
 
+    @Autowired
+    RestroomAmenityJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
+    @Test
+    void shouldAdd() {
+        RestroomAmenity agencyAmenity = makeRestroomAmenity();
+        assertTrue(repository.add(agencyAmenity));
+
+        try {
+            repository.add(agencyAmenity); // must fail
+            fail("cannot add an amenity to a restroom twice.");
+        } catch (DataAccessException ex) {
+            // this is expected.
+        }
+    }
+
+    @Test
+    void shouldDelete() {
+        assertTrue(repository.deleteByKey(1, 3));
+        assertFalse(repository.deleteByKey(1, 3));
+    }
+
+    RestroomAmenity makeRestroomAmenity() {
+        RestroomAmenity restroomAmenity = new RestroomAmenity();
+        restroomAmenity.setRestroomId(1);
+
+        Amenity amenity = new Amenity();
+        amenity.setAmenityId(6);
+        amenity.setAmenityName("Test");
+        restroomAmenity.setAmenity(amenity);
+        return restroomAmenity;
+    }
 }
