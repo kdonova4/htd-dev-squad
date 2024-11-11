02f5921b7d66eda0fbad58dd323706de96d9ae9e
Nick Murphy <nlimurphy@gmail.com>
2024-11-11 09:59:24 -0700

    added Restroom and Restroom Amenity models

--- server/toilet/src/main/java/learn/toilet/models/RestroomAmenity.java
+++ server/toilet/src/main/java/learn/toilet/models/RestroomAmenity.java
@@ -2,4 +2,23 @@ package learn.toilet.models;
 
 public class RestroomAmenity {
 
+    private int restroomId;
+    private Amenity amenity;
+
+    public int getRestroomId() {
+        return restroomId;
+    }
+
+    public void setRestroomId(int restroomId) {
+        this.restroomId = restroomId;
+    }
+
+    public Amenity getAmenity() {
+        return amenity;
+    }
+
+    public void setAmenity(Amenity amenity) {
+        this.amenity = amenity;
+    }
+
 }
