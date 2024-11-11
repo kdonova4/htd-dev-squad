00fe72dd8dfb41da80bd34bf539cfdb5f832897b
Stan Choi <sjc591@nyu.edu>
2024-11-11 12:25:03 -0500

    Add AppUser model and RestroomAmenity model

---
+++ server/toilet/src/main/java/learn/toilet/models/AppUser.java
@@ -0,0 +1,51 @@
+package learn.toilet.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AppUser extends User {
+
+    private static final String AUTHORITY_PREFIX = "ROLE_";
+
+    private int appUserId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public AppUser(int appUserId, String username, String password,
+                   boolean disabled, List<String> roles, String firstName, String lastName, String email) {
+        super(username, password, !disabled,
+                true, true, true,
+                convertRolesToAuthorities(roles));
+        this.appUserId = appUserId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public static List<GrantedAuthority> convertRolesToAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>(roles.size());
+        for (String role : roles) {
+            Assert.isTrue(!role.startsWith(AUTHORITY_PREFIX),
+                    () ->
+                            String.
+                                    format("%s cannot start with %s (it is automatically added)",
+                                            role, AUTHORITY_PREFIX));
+            authorities.add(new SimpleGrantedAuthority(AUTHORITY_PREFIX + role));
+        }
+        return authorities;
+    }
+
+    public static List<String> convertAuthoritiesToRoles(Collection<GrantedAuthority> authorities) {
+        return authorities.stream()
+                .map(a -> a.getAuthority().substring(AUTHORITY_PREFIX.length()))
+                .collect(Collectors.toList());
+    }
+}
--- server/toilet/src/main/java/learn/toilet/models/RestroomAmenity.java
+++ server/toilet/src/main/java/learn/toilet/models/RestroomAmenity.java
@@ -1,5 +1,22 @@
 package learn.toilet.models;
 
 public class RestroomAmenity {
+    private int restroomId;
+    private Amenity amenity;
 
+    public int getRestroomId() {
+        return restroomId;
+    }
+
+    public void setRestroomId(int restroomId) {
+        this.restroomId = restroomId;
+    }
+
+    public Amenity getAmenity() {
+        return amenity;
+    }
+
+    public void setAmenity(Amenity amenity) {
+        this.amenity = amenity;
+    }
 }
