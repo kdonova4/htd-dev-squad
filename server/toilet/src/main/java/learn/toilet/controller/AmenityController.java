45d671f697cf0bb206a0ce407cc3c4dbd58bb0ef
Stan Choi <sjc591@nyu.edu>
2024-11-12 04:32:37 -0500

    Add barebones Amenity controller

--- server/toilet/src/main/java/learn/toilet/controller/AmenityController.java
+++ server/toilet/src/main/java/learn/toilet/controller/AmenityController.java
@@ -1,4 +1,63 @@
 package learn.toilet.controller;
 
+import learn.toilet.domain.AmenityService;
+import learn.toilet.domain.Result;
+import learn.toilet.models.Amenity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/amenity")
 public class AmenityController {
+
+    private final AmenityService service;
+
+
+    public AmenityController(AmenityService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{amenityId}")
+    public ResponseEntity<Amenity> findById(@PathVariable int amenityId) {
+        Amenity amenity = service.findById(amenityId);
+        if (amenity == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(amenity);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Amenity amenity) {
+        Result<Amenity> result = service.add(amenity);
+        if(result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/{amenityId}")
+    public ResponseEntity<Object> update(@PathVariable int amenityId, @RequestBody Amenity amenity) {
+        if(amenityId != amenity.getAmenityId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Amenity> result = service.update(amenity);
+        if(result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{amenityId}")
+    public ResponseEntity<Object> deleteById(@PathVariable int amenityId) {
+        if(service.deleteById(amenityId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
