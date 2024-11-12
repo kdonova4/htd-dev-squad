6fe5b978675336c895009333b97147331bad18be
Stan Choi <sjc591@nyu.edu>
2024-11-12 12:41:10 -0500

    Remove unused imports

--- server/toilet/src/main/java/learn/toilet/security/SecurityConfig.java
+++ server/toilet/src/main/java/learn/toilet/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package learn.toilet.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
