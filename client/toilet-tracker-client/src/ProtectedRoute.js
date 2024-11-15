c8acb856652d69d0b8a43ed327a4ce06d42cce45
Stan Choi <sjc591@nyu.edu>
2024-11-15 10:09:14 -0500

    Navigate to login page if trying to add restroom as unlogged user, fix bug in nav bar which had unresponsive Add New Restroom link

--- client/toilet-tracker-client/src/ProtectedRoute.js
+++ client/toilet-tracker-client/src/ProtectedRoute.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, redirectTo, requireAuth }) => {
   const { token } = useAuth();
 
-  if (token) {
-    return <Navigate to="/" replace />;
+  if (requireAuth && !token) {
+    return <Navigate to={redirectTo || "/login"} replace />;
+  }
+
+  if (!requireAuth && token) {
+    return <Navigate to={redirectTo || "/"} replace />;
   }
 
   return children;
